01bff92097885b9b0a2ecf87821a68ef6bbffe2b
Vishal-Piprotar <piprotarvishal20@gmail.com>
2024-08-31 15:26:06 +0530

    [FIX] Validation For Book

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -7,6 +7,7 @@ public class Book {
     private final int publicationYear;
 
     public Book(String isbn, String title, String author, int publicationYear) {
+        validateRequiredAttributes(isbn, title, author, publicationYear);
         this.isbn = isbn;
         this.title = title;
         this.author = author;
