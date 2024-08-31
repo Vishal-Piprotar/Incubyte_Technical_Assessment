9c09af38289e8908f78d134c05c9cd19f8b5a2be
Vishal-Piprotar <piprotarvishal20@gmail.com>
2024-08-31 14:26:00 +0530

    Add Test annotation

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class LibraryTest {
+public class LibraryTest {
 
+    // This Test method Always pass
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
 }
